e2bab73e1f33096491b4042e4eacb2ffa391e334
Dominik Lubański <dominik.lubanski@gmail.com>
2022-04-29 15:43:11 +0200

    Run the onboarging on the first search

--- src/background.js
+++ src/background.js
@@ -1,10 +1,34 @@
 const VERSION = Number(browser.runtime.getManifest().version.split('.')[0]);
 
+let onboardingComplete = true;
+
 browser.storage.local.get(['version']).then(({ version = 0 }) => {
   if (version < VERSION) {
-    browser.tabs.create({
-      url: browser.runtime.getURL(`onboarding/index.html?version=${VERSION}`),
-      active: true,
-    });
+    onboardingComplete = false;
   }
+});
+
+browser.runtime.onMessage.addListener((message) => {
+  if (message === 'onboarding-complete') {
+    onboardingComplete = false;
+    browser.storage.local.set({ version: VERSION });
+    browser.webNavigation.onBeforeNavigate.removeListener(openOnboardingPage);
+  }
+});
+
+function openOnboardingPage({ tabId, url }) {
+  url = new URL(url);
+
+  browser.tabs.update(tabId, {
+    loadReplace: true,
+    url: browser.runtime.getURL(
+      `onboarding/index.html?version=${VERSION}&query=${
+        url.searchParams.get('q') || ''
+      }`,
+    ),
+  });
+}
+
+browser.webNavigation.onBeforeNavigate.addListener(openOnboardingPage, {
+  url: [{ urlContains: 'glowstery.com/search?q=' }],
 });
