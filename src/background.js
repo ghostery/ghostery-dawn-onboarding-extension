875ad9fe99d1559ad67959629ef2a9e6ce05d5d2
Dominik Lubański <dominik.lubanski@gmail.com>
2022-05-06 15:06:28 +0200

    Feat run on first search (#3)
    * Run the onboarging on the first search
    
    * Add prettier configuration

--- src/background.js
+++ src/background.js
@@ -1,10 +1,35 @@
 const VERSION = Number(browser.runtime.getManifest().version.split('.')[0]);
 
+let onboardingComplete = undefined;
+
 browser.storage.local.get(['version']).then(({ version = 0 }) => {
-  if (version < VERSION) {
-    browser.tabs.create({
-      url: browser.runtime.getURL(`onboarding/index.html?version=${VERSION}`),
-      active: true,
-    });
+  onboardingComplete = version === VERSION;
+});
+
+browser.runtime.onMessage.addListener((message) => {
+  if (message === 'onboarding-complete') {
+    onboardingComplete = true;
+    browser.storage.local.set({ version: VERSION });
+    browser.webNavigation.onBeforeNavigate.removeListener(openOnboardingPage);
   }
+});
+
+function openOnboardingPage({ tabId, url }) {
+  if (onboardingComplete) {
+    browser.webNavigation.onBeforeNavigate.removeListener(openOnboardingPage);
+    return;
+  }
+
+  browser.tabs.update(tabId, {
+    loadReplace: true,
+    url: browser.runtime.getURL(
+      `onboarding/index.html?version=${VERSION}&query=${
+        new URL(url).searchParams.get('q') || ''
+      }`,
+    ),
+  });
+}
+
+browser.webNavigation.onBeforeNavigate.addListener(openOnboardingPage, {
+  url: [{ urlContains: 'glowstery.com/search?q=' }],
 });
--- src/onboarding/index.js
+++ src/onboarding/index.js
@@ -1,4 +1,7 @@
-const version = Number((new URLSearchParams(window.location.search)).get('version'));
+const searchParams = new URLSearchParams(window.location.search);
+
+const version = Number(searchParams.get('version'));
+const query = searchParams.get('query');
 
 (function setupStep1() {
   const TIMER = 3000;
@@ -17,25 +20,44 @@ const version = Number((new URLSearchParams(window.location.search)).get('versio
 
 (function setupStep2() {
   async function submit() {
-    const searchEngineName = document.querySelector('.tab2 input:checked').value;
-    browser.ghostery.setDefaultSearchEngine(searchEngineName);
-    await browser.storage.local.set({ version });
+    const searchEngineName = document.querySelector(
+      '.tab2 input:checked',
+    ).value;
+
+    browser.ghostery?.setDefaultSearchEngine(searchEngineName);
+
+    await browser.runtime.sendMessage('onboarding-complete');
     const currentTab = await browser.tabs.getCurrent();
-    browser.tabs.create({});
-    browser.tabs.remove([currentTab.id]);
+
+    const searchTab = await browser.tabs.create({});
+
+    browser.search.search({
+      query,
+      engine: searchEngineName,
+      tabId: searchTab.id,
+    });
+
+    browser.tabs.remove(currentTab.id);
   }
+
   document.querySelector('.tab2 button').addEventListener('click', submit);
 
   document.querySelectorAll('input[name="search"]').forEach((elem) => {
     elem.addEventListener('change', (event) => {
       const id = event.target.id;
-      [...document.querySelectorAll('.description, .icon')].forEach(e => e.classList.add('hidden'));
-      [...document.querySelectorAll(`.description[data-description-for=${id}], .icon[for=${id}]`)].forEach(e => e.classList.remove('hidden'));
+      [...document.querySelectorAll('.description, .icon')].forEach((e) =>
+        e.classList.add('hidden'),
+      );
+      [
+        ...document.querySelectorAll(
+          `.description[data-description-for=${id}], .icon[for=${id}]`,
+        ),
+      ].forEach((e) => e.classList.remove('hidden'));
     });
   });
 
   (function selectFirstInput() {
-    const firstInput = document.querySelector('.tab2 input')
+    const firstInput = document.querySelector('.tab2 input');
     firstInput.checked = true;
     firstInput.dispatchEvent(new Event('change'));
   })();
