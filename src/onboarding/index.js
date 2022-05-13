17a069be8f547a0bd5efd19dd34f3b698bbd12a9
Dominik Lubański <dominik.lubanski@gmail.com>
2022-05-13 11:54:26 +0200

    Remove start tab content

--- src/onboarding/index.js
+++ src/onboarding/index.js
@@ -3,62 +3,41 @@ const searchParams = new URLSearchParams(window.location.search);
 const version = Number(searchParams.get('version'));
 const query = searchParams.get('query');
 
-(function setupStep1() {
-  const TIMER = 3000;
+async function submit() {
+  const searchEngineName = document.querySelector('input:checked').value;
 
-  const step1 = document.querySelector('.tab1');
-  const step2 = document.querySelector('.tab2');
+  browser.ghostery?.setDefaultSearchEngine(searchEngineName);
 
-  const timer = setTimeout(nextPage, TIMER);
+  await browser.runtime.sendMessage('onboarding-complete');
+  const currentTab = await browser.tabs.getCurrent();
 
-  function nextPage() {
-    clearInterval(timer);
-    step1.classList.add('hidden');
-    step2.classList.remove('hidden');
-  }
-})();
+  const searchTab = await browser.tabs.create({});
 
-(function setupStep2() {
-  async function submit() {
-    const searchEngineName = document.querySelector(
-      '.tab2 input:checked',
-    ).value;
-
-    browser.ghostery?.setDefaultSearchEngine(searchEngineName);
-
-    await browser.runtime.sendMessage('onboarding-complete');
-    const currentTab = await browser.tabs.getCurrent();
-
-    const searchTab = await browser.tabs.create({});
-
-    browser.search.search({
-      query,
-      engine: searchEngineName,
-      tabId: searchTab.id,
-    });
-
-    browser.tabs.remove(currentTab.id);
-  }
-
-  document.querySelector('.tab2 button').addEventListener('click', submit);
-
-  document.querySelectorAll('input[name="search"]').forEach((elem) => {
-    elem.addEventListener('change', (event) => {
-      const id = event.target.id;
-      [...document.querySelectorAll('.description, .icon')].forEach((e) =>
-        e.classList.add('hidden'),
-      );
-      [
-        ...document.querySelectorAll(
-          `.description[data-description-for=${id}], .icon[for=${id}]`,
-        ),
-      ].forEach((e) => e.classList.remove('hidden'));
-    });
+  browser.search.search({
+    query,
+    engine: searchEngineName,
+    tabId: searchTab.id,
   });
 
-  (function selectFirstInput() {
-    const firstInput = document.querySelector('.tab2 input');
-    firstInput.checked = true;
-    firstInput.dispatchEvent(new Event('change'));
-  })();
-})();
+  browser.tabs.remove(currentTab.id);
+}
+
+document.querySelector('button').addEventListener('click', submit);
+
+document.querySelectorAll('input[name="search"]').forEach((elem) => {
+  elem.addEventListener('change', (event) => {
+    const id = event.target.id;
+    [...document.querySelectorAll('.description, .icon')].forEach((e) =>
+      e.classList.add('hidden'),
+    );
+    [
+      ...document.querySelectorAll(
+        `.description[data-description-for=${id}], .icon[for=${id}]`,
+      ),
+    ].forEach((e) => e.classList.remove('hidden'));
+  });
+});
+
+const firstInput = document.querySelector('input');
+firstInput.checked = true;
+firstInput.dispatchEvent(new Event('change'));
